7bba938bd3f1b02ed9dccf544e6a0e0d4d6a0b3c
Your Name <you@example.com>
2017-03-16 16:35:46 +0100

    dossier trimage a etudier. j ai fini

--- tri.cpp
+++ tri.cpp
@@ -5,9 +5,9 @@
 
 int main(int argc, char** argv)
 {
-    if(argc < 3) {fprintf(stderr, "Please provide two mnist file\n"); exit(1); } // demande mise en boite de 2 fichiers
+    if(argc < 2) {fprintf(stderr, "Please provide two mnist file\n"); exit(1); } // demande mise en boite de 2 fichiers
     string path = argv[1];
-    string path_labels = argv[2]; //  2 arguments necessaires
+    string path_labels = argv[2] //  2 arguments necessaires
     float** data = read_mnist(path); // readmnist definie dns mnist.h//
     float* triage = read_labels(path_labels); 
     
