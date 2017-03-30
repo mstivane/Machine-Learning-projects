a7f265b68a099d88dfc941a0df86102715795c5f
Your Name <you@example.com>
2017-03-30 18:15:24 +0200

    projet fini : main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,62 @@
-
 #include "jpg.h"
 #include "mnist.h"
+#include "limits.h"
 
-
-
-int main(int argc, char** argv)
+float distance( int* v1, int*v2);
+float linear_classifier( float* w, float* x);
+int main()
 {
-    if(argc < 2) {fprintf(stderr, "Please provide a mnist file\n"); exit(1); }
-    string path = argv[1];
-
-    float** data = read_mnist(path); // readmnist definie dns mnist.h//
-
-    for(int i=0; i<10000; i++) {
-        printf("%u\n", i); // affichage i//
-        save_jpg(data[i], 28, 28, "%u/%04u.jpg", i/10000, i%10000); // savejpg definie dns jpg.h et %u ( ubytes)//
+    float** data = read_mnist("train-images.idx3-ubyte"); // readmnist definie dns mnist.h//
+    float* labels = read_labels("train-labels.idx1-ubyte"); 
+    float** test_images= read_mnist("t10k-images.idx3-ubyte");
+    float* test_labels= read_labels("t10k-labels.idx1-ubyte");
+    float E=0;
+    float* w=  new float[784];
+    for(int i=0; i<10000; i++) w[i]=(float)rand()*2/INT_MAX;
+    {
+         printf("%u\n", i);
+		printf("%f ERREUR\n", E);
+       float inference = linear_classifier(w, test_images[i]);
+       save_jpg(test_images[i], 28, 28, "%u/%04u.jpg", inference, i);
+       if(inference != test_labels[i])E++;
+		
+		{
+			printf("Erreur=%0.2f%%\n ", (E*100)/i);
+        }
+			
+		
     }
+
+
+     
     return 0;
+ }
+
+
+float distance(int* v1, int* v2)
+{
+	float d;
+
+	
+	for (int i=0; i<784; i++)
+	{
+		
+		d+= (v1[i]-v2[i])*(v1[i]-v2[i]);
+		
+       
+	}
+	
+	return d;
+}
+
+float linear_classifier( float* w, float* x)
+{
+
+ float d= 0; 
+for( int i =0; i<784; i++)
+{
+   d+= w[i]*x[i];
+}
+if(d>=0) return 1;
+else return 0;
 }
---
+++ src/trimage.cpp
@@ -0,0 +1,51 @@
+#include "jpg.h"
+#include "mnist.h"
+
+
+float distance( int* v1, int*v2);
+int main()
+{
+    float** data = read_mnist("train-images.idx3-ubyte"); // readmnist definie dns mnist.h//
+    float* labels = read_labels("train-labels.idx1-ubyte"); 
+    float** test_images= read_mnist("t10k-images.idx3-ubyte");
+         
+    for(int i=0; i<10000; i++) 
+    {
+        
+		float mind  = -1;
+		int NN;// +proche voisin //
+       for(int j=0; j<60000; j++) // comparaison image i et j//
+       {
+		   float distance1=  distance(test_images[i], data[j]);
+		   
+		   if(distance1<mind || mind == -1)
+		   {
+			   
+			   mind = distance1;
+			   NN = j;
+		   }
+		   
+	   }
+       int inference =  labels[NN];
+       save_jpg(test_images[i], 28, 28, "%u/%u.jpg", inference, i);
+    }
+    return 0;
+}
+
+
+float distance(int* v1, int* v2, float *nb)
+{
+	float d;
+    float min;
+
+	
+	for (int i=0; i<784; i++)
+	{
+		
+		d+= (v1[i]-v2[i])*(v1[i]-v2[i]);
+		
+       
+	}
+	
+	return d;
+}
